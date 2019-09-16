f41fdbda16f2c3be45f73896c5b7af11f6373187
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-16 01:28:04 +0000

    Automatically backed up by Learn

---
+++ example.rb
@@ -0,0 +1,8 @@
+def make_sandwich(element1, element2)
+  base = "A #{element1} and #{element2}"
+  yield(base)
+end
+
+make_sandwich("gator", "gumbo") do |innards|
+  "#{innards} on rye"
+end #=> "A gator and gumbo on rye"
